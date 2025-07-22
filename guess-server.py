df04aea400b00cb4ee7a5eac14c481279060eb12
Mike Smith <mike_smith@harvard.edu>
2025-07-22 16:49:00 -0400

    Fix some line wraps

--- guess-server.py
+++ guess-server.py
@@ -2,7 +2,7 @@
 import random
 from socket32 import create_new_socket
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+HOST = '127.0.0.1'  # Standard loopback interface address
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 def main():
